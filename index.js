f0e54350a95849450f777ee31cd3dfcee90e8d6f
himanshu2454 <hchouhan3654@gmail.com>
2020-08-16 20:09:39 +0530

    login frontend

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function btnfunc(){
     //console.log("1");
-    var email = document.getElementById("exampleInputEmail1").value;
-    var pass = document.getElementById("exampleInputPassword1").value;
+    let email = document.getElementById("exampleInputEmail1").value;
+    let pass = document.getElementById("exampleInputPassword1").value;
     // console.log("2");
-    if( email == "rajputshubham@gmail.com" && pass == "asdf"){
-        
-        window.location.replace("file:///D:/himanshu.chouhan/JavaScript/average.html");
+    if( email == 'rajputshubham@gmail.com' && pass == 'asdf'){
+        //alert("cooming");
+        window.open('file:///D:/himanshu.chouhan/JavaScript/average.html', '_blank');
+        //window.location = "file:///D:/himanshu.chouhan/JavaScript/average.html";
     }
    
 }
 
 function avgcal(form){
-   let ss = Number(form.s1.value);
-   alert(ss);
-    // let ss1 = Number(getElementById("s1").value);
-    // let ss2 = Number(getElementById("s2").value);
-    // let ss3 = Number(getElementById("s3").value)
-    // let ss4 = Number(getElementById("s4").value);
-    // let ss5 = Number(getElementById("s5").value);
+  
+    let ss1 = Number(document.getElementById("s1").value);
+    let ss2 = Number(document.getElementById("s2").value);
+    let ss3 = Number(document.getElementById("s3").value)
+    let ss4 = Number(document.getElementById("s4").value);
+    let ss5 = Number(document.getElementById("s5").value);
 
-    // let avg = (ss1 + ss2 + ss3 + ss4 + ss5) / 5;
-    // alert("average is" + avg );
+    let avg = (ss1 + ss2 + ss3 + ss4 + ss5) / 5;
+    alert("average is" + avg );
 
+}
+
+function calsum(){
+    //let ss1 = Number()
+    window.location = "file:///D:/himanshu.chouhan/JavaScript/average.html";
+    //alert("hlllo");
 }
